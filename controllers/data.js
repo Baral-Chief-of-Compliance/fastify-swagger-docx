5a8fa080cd9868c9a8a9f5506bec9fafeb0b01aa
Евгений Глущенко <dartmol21@mail.ru>
2022-08-16 23:30:20 +0300

    repair counter of duficult json

--- controllers/data.js
+++ controllers/data.js
@@ -1,6 +1,25 @@
 const postData = (req, reply) => {
     var count = Object.keys(req.body).length
-    reply.send({message: `number of pairs ${count}`})
+    
+    let counter = 0
+
+    function getFiniteValue(obj) {
+        getProp(obj);
+    
+        function getProp(o) {
+            for(var prop in o) {
+                if(typeof(o[prop]) === 'object') {
+                    getProp(o[prop]);
+                } else {
+                    console.log('Finite value: ',o[prop])
+                    counter++
+                }
+            }
+        }
+    }
+
+    getFiniteValue(req.body)
+    reply.send({message: `number of pairs ${counter}`})
 }
 
 module.exports = {
